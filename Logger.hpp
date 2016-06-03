79224518e6b70a9a5c24e809ae644419db19b8ce
Colin <colingilbert86@gmail.com>
2016-06-03 03:03:21 -0600

    Tweak

--- Logger.hpp
+++ Logger.hpp
@@ -13,7 +13,7 @@ namespace noob
 				CRITICAL = 0, ERROR = 1, WARNING = 2, EVENT = 3, INFO = 4, USER = 5
 			};
 
-			static void log(const std::string&, noob::logger::importance i = noob::logger::importance::INFO)
+			static void log(const std::string& message, noob::logger::importance i = noob::logger::importance::INFO)
 			{
 				std::cout << message << std::endl;
 			}
