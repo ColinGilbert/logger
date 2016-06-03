6c325fa2cb88d985a358c90022e88c237f878542
Colin <colingilbert86@gmail.com>
2016-06-03 02:43:20 -0600

    Initial commit

---
+++ Logger.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "format.h"
+#include <iostream>
+
+namespace noob
+{
+	class logger
+	{
+		public:
+			enum importance
+			{
+				CRITICAL = 0, ERROR = 1, WARNING = 2, EVENT = 3, INFO = 4, USER = 5
+			};
+
+			static void log(const std::string&, noob::logger::importance i = noob::logger::importance::INFO)
+			{
+				std::cout << message << std::endl;
+			}
+	};
+}
